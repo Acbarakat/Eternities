34e51758eb37498acb3498720e71d89b5c47e7ff
Allonte <angelosheltie@gmail.com>
2016-04-30 15:56:16 -0700

    Update the windows requirements batch file

--- requirements_win.bat
+++ requirements_win.bat
@@ -1,3 +1,5 @@
 python -m pip install -r requirements.txt --upgrade
 python -m pip install pypiwin32 kivy.deps.sdl2 kivy.deps.glew kivy.deps.gstreamer --extra-index-url https://kivy.org/downloads/packages/simple/ --upgrade
 python -m pip install kivy --upgrade
+garden install --update particlesystem
+cd %USERPROFILE%\.kivy\garden\garden.particlesystem\setup.py install
