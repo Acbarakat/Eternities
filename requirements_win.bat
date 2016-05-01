1918af13005cde5c95efcd6d7790173e7f3a93f7
Allonte <angelosheltie@gmail.com>
2016-05-01 12:09:33 -0700

    Use Visual Studio 2015 to compile particle system

--- requirements_win.bat
+++ requirements_win.bat
@@ -2,4 +2,6 @@ python -m pip install -r requirements.txt --upgrade
 python -m pip install pypiwin32 kivy.deps.sdl2 kivy.deps.glew kivy.deps.gstreamer --extra-index-url https://kivy.org/downloads/packages/simple/ --upgrade
 python -m pip install kivy --upgrade
 garden install --update particlesystem
-cd %USERPROFILE%\.kivy\garden\garden.particlesystem\setup.py install
+SET VS90COMNTOOLS=%VS140COMNTOOLS%
+cd %USERPROFILE%\.kivy\garden\garden.particlesystem\
+python setup.py install
