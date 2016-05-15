b53fc26dc22339455b60b44ba60845e597a91305
Allonte <angelosheltie@gmail.com>
2016-05-15 12:47:53 -0700

    Add in missing lin library for Pillow

--- requirements_lin.sh
+++ requirements_lin.sh
@@ -4,7 +4,9 @@ sudo add-apt-repository ppa:mc3man/trusty-media -y
 
 sudo apt-get update
 
-sudo apt-get install -y python-pip build-essential  git python python-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
+sudo apt-get install -y python-pip build-essential  git python python-dev 
+sudo apt-get install ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
+sudo apt-get install libjpeg8-dev
 
 sudo apt-get install -y python-kivy
 
